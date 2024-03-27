f588e3b82eb0b738e730d5e79d4f0de0406527d9
Valeroy <valeroy51@gmail.com>
2024-03-27 18:38:03 +0700

    merapihkan beberapa css

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 const ejs = require('ejs')
+const expresslayout = require('express-ejs-layouts')
 
 app.set("view engine", "ejs")
 
+// app.use(expresslayout)
+
 //index
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index'/*, {title : "Travel with Us", layout : "layout/main.ejs"}*/);
 })
 
 //tiket
 app.get('/tiket',(req, res) => {
-    res.render('tiket')
+    res.render('tiket');
 })
 
 //static
