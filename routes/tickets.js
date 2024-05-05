28ffb1cc4182e1758a8c97254bf66ae06030ac93
Stefanus Anthony <kangkung@gmail.com>
2024-05-05 22:23:11 +0700

    perbaikan nav & ticket ejs

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,10 +1,18 @@
-const router = require("express").Router();
-const Ticket = require("../Models/Ticket");
 const tickets = require("../files/tiket.json");
-const express = require('express');
 const multer = require('multer')
 const fs = require('fs');
-const { type } = require('os');
+const {type} = require('os');
+const express = require('express');
+const router = express.Router();
+const Ticket = require("../Models/Ticket");
+const session = require('express-session');
+
+// Middleare to check session
+router.use(session({
+    secret: 'your_secret_key', // ganti dengan kunci rahasia sesi Anda
+    resave: false,
+    saveUninitialized: true
+}));
 
 router.get('/add', (req, res) => {
     res.render('add_tickets', {title: "Add Tickets"});
@@ -12,76 +20,90 @@ router.get('/add', (req, res) => {
 
 // upload gambar
 var storage = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: function (req, file, cb) {
         cb(null, './uploads');
     },
-    filename: function(req, file, cb){
-        cb(null, file.fieldname + "_" + Date.now()+ "_" + file.originalname);
-    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "" + Date.now() + "" + file.originalname);
+    }
 });
 
-var upload = multer({
-    storage: storage,
-}).single('image');
-
+var upload = multer({storage: storage}).single('image');
 
 //Insert a tiket into database route
-router.post('/add', upload, (req, res) =>{
-    const tiket = new Ticket({
-        kota: req.body.kota,
-        negara: req.body.negara,
-        harga: req.body.harga,
-        benua: req.body.benua,
-        image: req.file.filename
-    });
+router.post('/add', upload, (req, res) => {
+    const tiket = new Ticket(
+        {kota: req.body.kota, negara: req.body.negara, harga: req.body.harga, benua: req.body.benua, image: req.file.filename}
+    );
 
-    tiket.save()
-    .then(() => {
-        req.session.message = {
-            type: 'success',
-            message: 'Ticket added successfully!'
-        };
-        res.redirect('/tiket');
-    })
-    .catch((err) => {
-        res.json({ message: err.message, type: 'danger' });
-    });
+    tiket
+        .save()
+        .then(() => {
+            req.session.message = {
+                type: 'success',
+                message: 'Ticket added successfully!'
+            };
+            res.redirect('/tiket');
+        })
+        .catch((err) => {
+            res.json({message: err.message, type: 'danger'});
+        });
 
 });
 
 // get all tickets route
 router.get('/tiket', (req, res) => {
-    Ticket.find().exec()
-    .then(ticket => {
-        res.render('admintiket', {
-            title: 'Tiket Page',
-            ticket: ticket,
-        });
-    }).catch(err =>{
-        res.json({message: err.message});
-    });
+    // Memeriksa apakah req.session.user terdefinisi
+    if (req.session.user) {
+        const username = req.session.user.username;
+        // Jika pengguna adalah admin, render 'admintiket'
+        if (username === 'admin') {
+            Ticket
+                .find()
+                .exec()
+                .then(ticket => {
+                    res.render('admintiket', {
+                        title: 'Tiket Page',
+                        ticket: ticket
+                    });
+                })
+                .catch(err => {
+                    res.json({message: err.message});
+                });
+        } else {
+            // Jika pengguna bukan admin, render 'tiket'
+            res.render('tiket', {
+                user: {
+                    username: username
+                }
+            });
+        }
+    } else {
+        // Handle the case where req.session.user is not defined
+        res.redirect('/login'); // Redirect to login page or handle as needed
+    }
 });
 
 // edit a ticket route
 router.get('/edit/:id', (req, res) => {
     let id = req.params.id;
-    Ticket.findById(id)
+    Ticket
+        .findById(id)
         .then(ticket => {
             if (!ticket) { // Mengganti "ticket == ull" dengan "!ticket"
-                res.redirect('/');
+                res.redirect('/tiket');
             } else {
                 res.render('edit_tickets', {
                     title: 'Edit Ticket',
-                    ticket: ticket,
+                    ticket: ticket
                 });
             }
         })
         .catch(err => {
             console.error(err);
-            res.redirect('/');
+            res.redirect('/tiket');
         });
 });
-
 
 router.post('/update/:id', upload, (req, res) => {
     let id = req.params.id;
@@ -98,28 +120,30 @@ router.post('/update/:id', upload, (req, res) => {
         new_image = req.body.old_image;
     }
 
-    Ticket.findByIdAndUpdate(id, {
+    Ticket
+        .findByIdAndUpdate(id, {
             kota: req.body.kota,
             negara: req.body.negara,
             harga: req.body.harga,
-            image: new_image,
+            image: new_image
         })
         .then(result => {
             req.session.message = {
                 type: 'success',
-                message: 'Ticket updated successfully!',
+                message: 'Ticket updated successfully!'
             };
             res.redirect("/");
         })
         .catch(err => {
-            res.json({ message: err.message, type: 'danger' });
+            res.json({message: err.message, type: 'danger'});
         });
 });
 
 // Delete ticket route
 router.get('/delete/:id', (req, res) => {
     let id = req.params.id;
-    Ticket.findByIdAndDelete(id)
+    Ticket
+        .findByIdAndDelete(id)
         .then(result => {
             if (result.image != '') {
                 try {
@@ -135,7 +159,7 @@ router.get('/delete/:id', (req, res) => {
             res.redirect('/');
         })
         .catch(err => {
-            res.json({ message: err.message });
+            res.json({message: err.message});
         });
 });
 
@@ -210,15 +234,20 @@ router.get("/tiket", async (req, res) => {
 
 const insertTickets = async () => {
     try {
-        // Ambil semua nama tiket yang sudah ada di database
-        const existingTicketNames = await Ticket.distinct("name");
+        // Ambil semua tiket yang sudah ada di database
+        const existingTickets = await Ticket.find();
 
         // Filter data tiket yang belum ada di database
-        const newTickets = tickets.filter(ticket => !existingTicketNames.includes(ticket.name));
+        const newTickets = tickets.filter(newTicket => {
+            return !existingTickets.some(existingTicket => {
+                // Lakukan pengecekan kesamaan berdasarkan atribut-atribut tiket
+                return newTicket.name === existingTicket.name && newTicket.kota === existingTicket.kota && newTicket.negara === existingTicket.negara && newTicket.harga === existingTicket.harga && newTicket.benua === existingTicket.benua;
+            });
+        });
 
         // Jika ada tiket baru, masukkan ke database
         if (newTickets.length > 0) {
-            const docs = await Ticket.insertMany(newTickets);
+            const docs = await Ticket.insertMany(newTickets, {ordered: false});
             console.log("Data baru telah ditambahkan ke database.");
             return Promise.resolve(docs);
         } else {
